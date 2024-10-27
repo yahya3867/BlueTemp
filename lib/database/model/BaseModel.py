960580ed43b08c19e93f7b214162b35750974f62
Theodore Spanbauer <theodorespanbauer@gmail.com>
2024-10-27 15:01:13 -0400

    Feature: Added database model classes to database submodule

---
+++ lib/database/model/BaseModel.py
@@ -0,0 +1,11 @@
+"""Module serving as the Base Model for all the
+database dataclasses"""
+
+__all__ = ["BaseModel"]
+
+
+class BaseModel:
+    """The base dataclass for the database tables."""
+
+    def __init__():
+        super().__init__()
---
+++ lib/database/model/__init__.py
@@ -0,0 +1,11 @@
+"""Module for storing the database model classes"""
+# Location
+# lib\database\model\__init__.py
+
+# Files
+from .BaseModel import BaseModel
+from .SensorDevice import SensorDevice
+from .SensorReading import SensorReading
+
+
+__all__ = [SensorDevice]
