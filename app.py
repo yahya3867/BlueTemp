23bc2b87ef796544267ad3e020faf0d8730d68d4
jglsung <jglsung02@gmail.com>
2024-11-01 03:31:19 -0400

    Fix: flask host for proper networking between docker and host

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from flaskr import create_app
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, host="localhost", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
